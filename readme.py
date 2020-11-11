229a20da70760b8d7cb8f8e709fca7523d870bbd
harith md noor <harithmdnoor@gmail.com>
2020-11-11 14:45:43 +0800

    #5

--- readme.py
+++ readme.py
@@ -1,4 +1,6 @@
 
-print ('Wednesday', '11 Nov 2020') //day,date,year
+print ('Wednesday', '11 Nov 2020') #day,date,year
 
-print('Team 3') //Team name
+print('Team 3') #Team name
+
+memberList = ['Harith','John','Hannanis','Guhesh']
